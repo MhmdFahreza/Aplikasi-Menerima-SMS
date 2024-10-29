754e992580e0fc06d9fb3e8d18bcc4b21eb924ee
Muhammad Fahreza <yya77591@gmail.com>
2024-10-29 17:56:08 +0700

    Nambahin Fitur Download dan muncul notif

--- app/src/main/java/com/example/mybroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/example/mybroadcastreceiver/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.mybroadcastreceiver
 
 import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -10,19 +16,24 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.mybroadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    companion object {
+        const val ACTION_DOWNLOAD_STATUS = "download_status"
+    }
 
+    private lateinit var downloadReceiver: BroadcastReceiver
     private var binding: ActivityMainBinding? = null
-    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>  // Declare the requestPermissionLauncher
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-        binding?.btnPermission?.setOnClickListener(this)
 
-        // Inisialisasi requestPermissionLauncher
-        requestPermissionLauncher = registerForActivityResult(
+        binding?.btnPermission?.setOnClickListener(this)
+        binding?.btnDownload?.setOnClickListener(this)
+
+        requestPermissionLauncher = registerForActivityResult(  // Initialize it in onCreate
             ActivityResultContracts.RequestPermission()
         ) { isGranted: Boolean ->
             if (isGranted) {
@@ -31,16 +42,35 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
             }
         }
+
+        downloadReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                Toast.makeText(context, "Download Selesai", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val downloadIntentFilter = IntentFilter(ACTION_DOWNLOAD_STATUS)
+        registerReceiver(downloadReceiver, downloadIntentFilter)
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_permission -> requestPermissionLauncher.launch(Manifest.permission.RECEIVE_SMS)
+            R.id.btn_download -> {
+                //simulate download process in 3 seconds
+                Handler(Looper.getMainLooper()).postDelayed(
+                    {
+                        val notifyFinishIntent = Intent().setAction(ACTION_DOWNLOAD_STATUS)
+                        sendBroadcast(notifyFinishIntent)
+                    },
+                    3000
+                )
+            }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        unregisterReceiver(downloadReceiver)
         binding = null
     }
 }
